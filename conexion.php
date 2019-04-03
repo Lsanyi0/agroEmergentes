83b414dd5d79ecb5125c399f215e64a53b896cec
Mario Cabrera <MaritoCx.mcabreralinares@gmail.com>
2019-04-03 02:28:07 -0600

    conexion v0.1

--- conexion.php
+++ conexion.php
@@ -1,9 +1,24 @@
 <?php 
 $conn = mysqli_connect("127.0.0.1", "root", "", "LasPalmeras");
-if ("$conn->connet_error") {
-    
-}
+function openCon (){
+if ($conn->connect_error)
+    die($conn->connect_error);
+
+print "Conexión exitosa!\n";
 
 exit(1);
+}
+function closeCon (){
+    mysqli_close($conn);
+}
+function Login (){
+openCon();
+$usuario=$_GET["usuario"];
+$contrasena=$_GET["contrasena"];
+    $sql ="Select Login();";
+    $q = $conn->query($sql);
+    $q->setFetchMode
+
+}
 
 ?>
